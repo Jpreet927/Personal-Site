778db5314515e160479f28153e9d7702aabd6e4a
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-03-27 22:08:35 -0400

    added navbar background on scroll

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // https://stackoverflow.com/questions/39042701/box-shadow-color-based-on-colors-from-image
+
+function toggleNavbarBackground() {
+    let navbar = document.querySelector('.main__navbar');
+    window.pageYOffset > 900 ? navbar.classList.add('navbar-background') : navbar.classList.remove('navbar-background')
+}
+
+window.addEventListener('scroll', () => {
+    toggleNavbarBackground()
+});
