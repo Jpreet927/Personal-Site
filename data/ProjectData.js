0215cc963e0a298dcaac46065f2d73636d21ab8b
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-03-28 10:57:49 -0400

    added projects section on home page

---
+++ data/ProjectData.js
@@ -0,0 +1,23 @@
+const projects = {
+    harmoni: {
+        preview: "",
+        title: "",
+        category: "",
+        description: "",
+        link: "",
+    },
+    entropyGaming: {
+        preview: "",
+        title: "",
+        category: "",
+        description: "",
+        link: "",
+    },
+    chicagoHuntsmen: {
+        preview: "",
+        title: "",
+        category: "",
+        description: "",
+        link: "",
+    }
+}
