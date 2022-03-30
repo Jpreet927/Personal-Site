dbbeace2be691552e245e6e9e6ff760cb91683dc
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-03-30 14:03:34 -0400

    hero section animation

---
+++ data/GalleryData.js
@@ -0,0 +1,97 @@
+const softwareProjects = {
+    cvBuilder = {
+        previewImg: "",
+        title: "",
+        description: "",
+        preview: "",
+        repo: "",
+    },
+    weatherApp = {
+        previewImg: "",
+        title: "",
+        description: "",
+        preview: "",
+        repo: "",
+    },
+    todoList = {
+        previewImg: "",
+        title: "",
+        description: "",
+        preview: "",
+        repo: "",
+    }
+}
+
+const designProjects = {
+    entropy = {
+        previewImg: "",
+        title: "",
+        description: "",
+        behanceLink: "",
+    },
+    huntsmen = {
+        previewImg: "",
+        title: "",
+        description: "",
+        behanceLink: "",
+    },
+    rush = {
+        previewImg: "",
+        title: "",
+        description: "",
+        behanceLink: "",
+    },
+}
+
+const artwork = {
+    paths = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    codeGeass = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    lastOfUs2 = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    strangerThings = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    donda = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    interframe = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    interframe = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    empireScore = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    geurillasGameday = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+    ravensScore = {
+        previewImg: "",
+        title: "",
+        description: ""
+    },
+}
--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 // https://stackoverflow.com/questions/39042701/box-shadow-color-based-on-colors-from-image
 
+let tl = gsap.timeline({ defaults: { ease: "power4.easeOut", duration: 2 } });
+
+tl.from('.main__container', { scale: 1.15, duration: 1, opacity: 0 })
+    .from('.main__body-header', { y: 100, opacity: 0, duration: 1 }, "-=0.75")
+    .from('.main__body-subtitle', { y: 75, opacity: 0, duration: 1 }, "-=0.75")
+    .from('#twitter-logo', { y: 50, opacity: 0, duration: 0.5 }, "-=0.7")
+    .from('#instagram-logo', { y: 50, opacity: 0, duration: 0.5 }, "-=0.6")
+    .from('#github-logo', { y: 50, opacity: 0, duration: 0.5 }, "-=0.5")
+    .from('#behance-logo', { y: 50, opacity: 0, duration: 0.5 }, "-=0.4")
+    .from('.main__social-line', { height: 0, duration: 1.25 }, "-=1")
+
+
+// CALLBACK FUNCTIONS
+
 function toggleNavbarBackground() {
     let navbar = document.querySelector('.main__navbar');
     window.pageYOffset > 900 ? navbar.classList.add('navbar-background') : navbar.classList.remove('navbar-background')
 }
+
+// EVENT LISTENERS
 
 window.addEventListener('scroll', () => {
     toggleNavbarBackground()
