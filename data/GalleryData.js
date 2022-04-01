6b4a2279c09d48d31488d87183fa8e83affe335e
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-04-01 09:19:23 -0400

    dynamically generating gallery images

--- data/GalleryData.js
+++ data/GalleryData.js
@@ -1,22 +1,22 @@
 const softwareProjects = {
     cvBuilder = {
-        previewImg: "",
-        title: "",
-        description: "",
+        previewImg: "../images/Gallery Thumbnails/Development/CV-App.png",
+        title: "React CV Builder",
+        description: "A platform to input personal info and work history to generate a Resume.",
         preview: "",
         repo: "",
     },
     weatherApp = {
-        previewImg: "",
-        title: "",
-        description: "",
+        previewImg: "../images/Gallery Thumbnails/Development/Weather-App.png",
+        title: "Dynamic Weather App",
+        description: "A webapp that fetches weather data from the Open Weather API with dynamic icons and background, reflecting the current climate.",
         preview: "",
         repo: "",
     },
     todoList = {
-        previewImg: "",
-        title: "",
-        description: "",
+        previewImg: "../images/Gallery Thumbnails/Development/To-Do-List.png",
+        title: "To-Do List",
+        description: "A subject organized CRUD task list where topics are assigned a randomly generated colour identifier.",
         preview: "",
         repo: "",
     }
@@ -24,70 +24,70 @@ const softwareProjects = {
 
 const designProjects = {
     entropy = {
-        previewImg: "",
-        title: "",
-        description: "",
-        behanceLink: "",
+        previewImg: "../images/Gallery Thumbnails/Design/Entropy-Rebrand.png",
+        title: "Entropy Gaming Brand Identity",
+        description: "Established new social media assets and brand guidelines to reflect core brand attributes.",
+        behanceLink: "https://www.behance.net/jaipreet",
     },
     huntsmen = {
-        previewImg: "",
-        title: "",
-        description: "",
-        behanceLink: "",
+        previewImg: "../images/Gallery Thumbnails/Design/Huntsmen-Proposal.png",
+        title: "Chicago Huntsmen Rebrand Proposal",
+        description: "Leveraged the established Huntsmen identity to bring a new take on the brands social media graphics.",
+        behanceLink: "https://www.behance.net/gallery/110895407/Chicago-Huntsmen",
     },
     rush = {
-        previewImg: "",
-        title: "",
-        description: "",
-        behanceLink: "",
+        previewImg: "../images/Gallery Thumbnails/Design/Rush-Rebrand.png",
+        title: "Rush Gaming Brand Refresh",
+        description: "Built on top of Rush Gamings core values to invoke a streetwear inspired visual identity.",
+        behanceLink: "https://www.behance.net/gallery/116128699/Rush-Gaming",
     },
 }
 
 const artwork = {
     paths = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/PATHS.png",
+        title: "PATHS",
+        description: "A piece inspired by Attack on Titan."
     },
     codeGeass = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/Code-Geass-Painting-Final.png",
+        title: "Emporer Lelouch",
+        description: "A piece inspured by Code Geass."
     },
     lastOfUs2 = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/Last-of-Us-2-Poster-2-updated.png",
+        title: "The Last of Us 2 Poster Concept",
+        description: "A poster concept for The Last of Us 2, inspired by the box art of the first installment."
     },
     strangerThings = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/Stranger-Things-3-Poster.png",
+        title: "Stranger Things 3 Poster Concept",
+        description: "A poster concept for Stranger Things Season 3, inspired by elements of the show."
     },
     donda = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/Kanye West - DONDA joined.png",
+        title: "Kanye West - DONDA Album Art Concept",
+        description: "Inspired by various elements of the album and the marketing campaign."
     },
     interframe = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/Interframe - everything will be ok.png",
+        title: "Everything Will Be Ok.",
+        description: "An album art concept created for revolutionary musician and composer 'Interframe'"
     },
-    interframe = {
-        previewImg: "",
-        title: "",
-        description: ""
+    kyleLowryPoster = {
+        previewImg: "../images/Gallery Thumbnails/Artwork/Kyle-Lowry-Poster.png",
+        title: "Kyle Lowry, GROAT",
+        description: "A tribute to the former Toronto Raptor, Kyle Lowry."
     },
     empireScore = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/Dallas-Empire-Rome-3.png",
+        title: "Dallas Empire Scorecard",
+        description: "A scorecard concept created for competitive gaming organization Dallas Empire."
     },
     geurillasGameday = {
-        previewImg: "",
-        title: "",
-        description: ""
+        previewImg: "../images/Gallery Thumbnails/Artwork/LA-Guerrilas-Gameday.png",
+        title: "LA Guerillas Gameday",
+        description: "A gameday concept created for competitive gaming organization LA Guerillas."
     },
     ravensScore = {
         previewImg: "",
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// https://stackoverflow.com/questions/39042701/box-shadow-color-based-on-colors-from-image
+// import Scrollbar from 'smooth-scrollbar';
 
+// Smooth Scroll 
+// Scrollbar.init(document.querySelector('#body'));
+
+// GSAP Animations
 let tl = gsap.timeline({ defaults: { ease: "power4.easeOut", duration: 2 } });
 
 tl.from('.main__container', { scale: 1.15, duration: 1, opacity: 0 })
@@ -12,15 +16,17 @@ tl.from('.main__container', { scale: 1.15, duration: 1, opacity: 0 })
     .from('.main__social-line', { height: 0, duration: 1.25 }, "-=1")
 
 
-// CALLBACK FUNCTIONS
+// Callback Functions
 
 function toggleNavbarBackground() {
     let navbar = document.querySelector('.main__navbar');
     window.pageYOffset > 900 ? navbar.classList.add('navbar-background') : navbar.classList.remove('navbar-background')
 }
 
-// EVENT LISTENERS
+// Event Listeners
 
 window.addEventListener('scroll', () => {
     toggleNavbarBackground()
 });
+
+// https://stackoverflow.com/questions/39042701/box-shadow-color-based-on-colors-from-image
