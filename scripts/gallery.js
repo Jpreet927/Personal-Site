670c8fa4dff07a173837ba6e8501b484e3181175
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-04-02 09:54:00 -0400

    changed art galley layout

---
+++ scripts/gallery.js
@@ -0,0 +1,6 @@
+// document.querySelectorAll(".development-project").forEach((project) => {
+//     project.addEventListener('mouseover', (e) => {
+//         console.log(e);
+//         project.firstChild.styles.display = "block";
+//     })
+// })
