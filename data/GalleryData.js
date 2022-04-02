b229109faef64cc36ea6ef7614962b7512dd3741
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-04-02 16:13:05 -0400

    rendered gallery with js, added enlarged image functionality

--- data/GalleryData.js
+++ data/GalleryData.js
@@ -1,19 +1,19 @@
-const softwareProjects = {
-    cvBuilder = {
+export const softwareProjects = {
+    cvBuilder: {
         previewImg: "../images/Gallery Thumbnails/Development/CV-App.png",
         title: "React CV Builder",
         description: "A platform to input personal info and work history to generate a Resume.",
         preview: "",
         repo: "",
     },
-    weatherApp = {
+    weatherApp: {
         previewImg: "../images/Gallery Thumbnails/Development/Weather-App.png",
         title: "Dynamic Weather App",
         description: "A webapp that fetches weather data from the Open Weather API with dynamic icons and background, reflecting the current climate.",
         preview: "",
         repo: "",
     },
-    todoList = {
+    todoList: {
         previewImg: "../images/Gallery Thumbnails/Development/To-Do-List.png",
         title: "To-Do List",
         description: "A subject organized CRUD task list where topics are assigned a randomly generated colour identifier.",
@@ -22,20 +22,20 @@ const softwareProjects = {
     }
 }
 
-const designProjects = {
-    entropy = {
+export const designProjects = {
+    entropy: {
         previewImg: "../images/Gallery Thumbnails/Design/Entropy-Rebrand.png",
         title: "Entropy Gaming Brand Identity",
         description: "Established new social media assets and brand guidelines to reflect core brand attributes.",
         behanceLink: "https://www.behance.net/jaipreet",
     },
-    huntsmen = {
+    huntsmen: {
         previewImg: "../images/Gallery Thumbnails/Design/Huntsmen-Proposal.png",
         title: "Chicago Huntsmen Rebrand Proposal",
         description: "Leveraged the established Huntsmen identity to bring a new take on the brands social media graphics.",
         behanceLink: "https://www.behance.net/gallery/110895407/Chicago-Huntsmen",
     },
-    rush = {
+    rush: {
         previewImg: "../images/Gallery Thumbnails/Design/Rush-Rebrand.png",
         title: "Rush Gaming Brand Refresh",
         description: "Built on top of Rush Gamings core values to invoke a streetwear inspired visual identity.",
@@ -43,55 +43,57 @@ const designProjects = {
     },
 }
 
-const artwork = {
-    paths = {
+export const artwork = {
+    paths: {
         previewImg: "../images/Gallery Thumbnails/Artwork/PATHS.png",
         title: "PATHS",
         description: "A piece inspired by Attack on Titan."
     },
-    codeGeass = {
+    codeGeass: {
         previewImg: "../images/Gallery Thumbnails/Artwork/Code-Geass-Painting-Final.png",
         title: "Emporer Lelouch",
         description: "A piece inspured by Code Geass."
     },
-    lastOfUs2 = {
+    lastOfUs2: {
         previewImg: "../images/Gallery Thumbnails/Artwork/Last-of-Us-2-Poster-2-updated.png",
         title: "The Last of Us 2 Poster Concept",
         description: "A poster concept for The Last of Us 2, inspired by the box art of the first installment."
     },
-    strangerThings = {
+    strangerThings: {
         previewImg: "../images/Gallery Thumbnails/Artwork/Stranger-Things-3-Poster.png",
         title: "Stranger Things 3 Poster Concept",
         description: "A poster concept for Stranger Things Season 3, inspired by elements of the show."
     },
-    donda = {
+    donda: {
         previewImg: "../images/Gallery Thumbnails/Artwork/Kanye West - DONDA joined.png",
         title: "Kanye West - DONDA Album Art Concept",
         description: "Inspired by various elements of the album and the marketing campaign."
     },
-    interframe = {
+    interframe: {
         previewImg: "../images/Gallery Thumbnails/Artwork/Interframe - everything will be ok.png",
         title: "Everything Will Be Ok.",
         description: "An album art concept created for revolutionary musician and composer 'Interframe'"
     },
-    kyleLowryPoster = {
+    kyleLowryPoster: {
         previewImg: "../images/Gallery Thumbnails/Artwork/Kyle-Lowry-Poster.png",
         title: "Kyle Lowry, GROAT",
         description: "A tribute to the former Toronto Raptor, Kyle Lowry."
     },
-    empireScore = {
+    empireScore: {
         previewImg: "../images/Gallery Thumbnails/Artwork/Dallas-Empire-Rome-3.png",
         title: "Dallas Empire Scorecard",
-        description: "A scorecard concept created for competitive gaming organization Dallas Empire."
+        description: "A scorecard graphic concept created for competitive gaming organization Dallas Empire."
     },
-    geurillasGameday = {
+    geurillasGameday: {
         previewImg: "../images/Gallery Thumbnails/Artwork/LA-Guerrilas-Gameday.png",
         title: "LA Guerillas Gameday",
-        description: "A gameday concept created for competitive gaming organization LA Guerillas."
+        description: "A gameday graphic concept created for competitive gaming organization LA Guerillas."
     },
-    ravensScore = {
-        previewImg: "",
-        title: "",
-        description: ""
+    ravensScore: {
+        previewImg: "../images/Gallery Thumbnails/Artwork/LRR-Victory.png",
+        title: "London Royal Ravens Victory",
+        description: "A match result graphic concept created for competitive gaming organization London Royal Ravens."
     },
 }
+
+// export { softwareProjects, designProjects, artwork }
--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,6 +1,181 @@
-// document.querySelectorAll(".development-project").forEach((project) => {
-//     project.addEventListener('mouseover', (e) => {
-//         console.log(e);
-//         project.firstChild.styles.display = "block";
-//     })
-// })
+import { softwareProjects, designProjects, artwork } from "../data/GalleryData.js";
+
+let enlargeCloseBtn = document.getElementById("gallery-enlarge-close");
+let enlargeContainer = document.querySelector(".gallery-enlarge-container");
+let galleryContainer = document.querySelector(".gallery-main-container");
+
+// render software projects
+function renderSoftwareProjects() {
+    let container = document.querySelector(".development-projects-container");
+    console.log("if ur reading this i love u")
+    
+    for (let key in softwareProjects) {
+        let project = softwareProjects[key];
+
+        let projectContainer = document.createElement("div");
+        projectContainer.classList.add("development-project");
+
+        let projectThumbnail = document.createElement("img");
+        projectThumbnail.src = project.previewImg;
+        projectThumbnail.alt = `${project.title} thumbnail.`;
+
+        let projectInfoContainer = document.createElement("div");
+        projectInfoContainer.classList.add("project-information");
+
+        let projectInfoDetails = document.createElement("div");
+        projectInfoDetails.classList.add("project-information-details");
+
+        let projectInfoTitle = document.createElement("h5");
+        projectInfoTitle.textContent = project.title;
+        projectInfoTitle.classList.add("project-information-title");
+
+        let projectInfoDescription = document.createElement("p");
+        projectInfoDescription.textContent = project.description;
+        projectInfoDescription.classList.add("project-information-description");
+
+        let projectLinksContainer = document.createElement("div");
+        projectLinksContainer.classList.add("project-preview");
+
+        let projectPreviewLink = document.createElement("a");
+        projectPreviewLink.textContent = "Live Preview";
+        projectPreviewLink.href = project.preview;
+        projectPreviewLink.target = "_blank";
+
+        let projectRepoLink = document.createElement("a");
+        projectRepoLink.textContent = "Source Code";
+        projectRepoLink.href = project.repo;
+        projectRepoLink.target = "_blank";
+
+        let projectLinkSeparator = document.createElement("p");
+        projectLinkSeparator.textContent = "|";
+
+        projectLinksContainer.append(projectPreviewLink, projectLinkSeparator, projectRepoLink);
+        projectInfoDetails.append(projectInfoTitle, projectInfoDescription, projectLinksContainer);
+        projectInfoContainer.append(projectInfoDetails);
+        projectContainer.append(projectInfoContainer, projectThumbnail);
+        container.append(projectContainer);
+    }
+}
+
+// render design projects
+function renderDesignProjects() {
+    let container = document.querySelector(".design-projects-container");
+
+    for (let key in designProjects) {
+        let project = designProjects[key];
+
+        let projectContainer = document.createElement("div");
+        projectContainer.classList.add("design-project");
+
+        let projectThumbnail = document.createElement("img");
+        projectThumbnail.src = project.previewImg;
+        projectThumbnail.alt = `${project.title} thumbnail.`;
+
+        let projectInfo = document.createElement("div");
+        projectInfo.classList.add("design-project-information");
+
+        let projectInfoDetails = document.createElement("div");
+        projectInfoDetails.classList.add("design-project-information-details");
+
+        let projectInfoTitle = document.createElement("h5");
+        projectInfoTitle.textContent = project.title;
+        projectInfoTitle.classList.add("design-project-information-title");
+
+        let projectInfoDescription = document.createElement("p");
+        projectInfoDescription.textContent = project.description;
+        projectInfoDescription.classList.add("design-project-information-description");
+
+        let projectLinksContainer = document.createElement("div");
+        let projectBehanceLink = document.createElement("a");
+        projectLinksContainer.classList.add("design-project-preview");
+        projectBehanceLink.textContent = "Link to Project";
+        projectBehanceLink.href = project.behanceLink;
+        projectBehanceLink.target = "_blank";
+
+        projectLinksContainer.append(projectBehanceLink);
+        projectInfo.append(projectInfoTitle, projectInfoDescription, projectLinksContainer);
+
+        projectContainer.append(projectInfo, projectThumbnail);
+        container.append(projectContainer);
+    }
+}
+
+// render artwork
+function renderArtwork() {
+    let column1 = document.getElementById("column-1");
+    let column2 = document.getElementById("column-2");
+    let column3 = document.getElementById("column-3");
+    let counter = 0;
+
+    for (let key in artwork) {
+        let image = artwork[key];
+
+        let imageContainer = document.createElement("div");
+        imageContainer.classList.add("artwork");
+
+        let imageThumbnail = document.createElement("img");
+        imageThumbnail.src = image.previewImg;
+        imageThumbnail.alt = `${image.title} thumbnail.`;
+
+        let imageEnlargeBtn = document.createElement("button");
+        imageEnlargeBtn.classList.add("artwork-enlarge-button");
+        imageEnlargeBtn.textContent = "Click To Enlarge";
+
+        let imageInformationContainer = document.createElement("div");
+        imageInformationContainer.classList.add("artwork-information");
+
+        let imageInformation = document.createElement("div");
+        imageInformation.classList.add("artwork-information-details");
+
+        let imageInformationTitle = document.createElement("h5");
+        imageInformationTitle.classList.add("artwork-information-title");
+        imageInformationTitle.textContent = image.title;
+
+        let imageInformationDescription = document.createElement("p");
+        imageInformationDescription.classList.add("artwork-information-description");
+        imageInformationDescription.textContent = image.description;
+
+        imageInformation.append(imageInformationTitle, imageInformationDescription);
+        imageInformationContainer.append(imageEnlargeBtn, imageInformation);
+        imageContainer.append(imageInformationContainer, imageThumbnail);
+
+        imageEnlargeBtn.addEventListener('click', () => {
+            localStorage.setItem('scrollPosition', window.scrollY);
+
+            let enlargeImageContainer = document.querySelector(".gallery-enlarge-image");
+            let enlargedImage = document.createElement("img");
+            enlargedImage.src = image.previewImg;
+
+            enlargeContainer.style.display = "block";
+            galleryContainer.style.display = "none";
+            enlargeImageContainer.innerHTML = "";
+            enlargeImageContainer.appendChild(enlargedImage);
+        })
+
+        if (counter % 3 == 0) {
+            column1.appendChild(imageContainer);
+        } else if (counter % 3 == 1) {
+            column2.appendChild(imageContainer);
+        } else {
+            column3.appendChild(imageContainer);
+        }
+
+        counter++;
+    }
+}
+
+enlargeCloseBtn.addEventListener('click', (e) => {
+    enlargeContainer.style.display = "none";
+    galleryContainer.style.display = "block";
+
+    let scrollPosition = localStorage.getItem('scrollPosition');
+    if (scrollPosition) {
+        window.scrollTo(0, scrollPosition);
+    } 
+});
+
+(function renderProjects() {
+    renderSoftwareProjects();
+    renderDesignProjects();
+    renderArtwork();
+})()
