b2c38dc81e5628a7c6e1d3a5f07ebc7d77456bbf
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-04-06 23:36:29 -0400

    Updates

--- pages/about.2a1e03a5.js
+++
@@ -1,8 +0,0 @@
-let hamburgerMenu = document.querySelector(".about-hamburger");
-let navbar = document.querySelector(".about-navbar ul");
-hamburgerMenu.addEventListener('click', ()=>{
-    hamburgerMenu.classList.toggle("visible");
-    navbar.classList.toggle("visible");
-});
-
-//# sourceMappingURL=about.2a1e03a5.js.map
--- pages/about.a31a495d.js
+++
@@ -1,8 +0,0 @@
-let hamburgerMenu = document.querySelector(".about-hamburger");
-let navbar = document.querySelector(".about-navbar ul");
-hamburgerMenu.addEventListener('click', ()=>{
-    hamburgerMenu.classList.toggle("visible");
-    navbar.classList.toggle("visible");
-});
-
-//# sourceMappingURL=about.a31a495d.js.map
